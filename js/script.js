0c27c5394cbae1c0a6f9c0fb3bde3116e2a38167
hridoy-rafayel <hridoy.rafayel@gmail.com>
2022-07-05 05:49:17 +0600

    slider area going on

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* ====== Main CSS ====== */
+var swiper = new Swiper(".bg-slider-thumb", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".bg-slider", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
