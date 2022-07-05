1bc6d9f6d217eb59f349bd1f718a608c0d93f07d
hridoy-rafayel <hridoy.rafayel@gmail.com>
2022-07-05 07:44:15 +0600

    slider & media completed

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
-/* ====== Main CSS ====== */
-var swiper = new Swiper(".bg-slider-thumb", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
+/* ======  ====== */
+var swiper = new Swiper(".bg-slider-thumbs", {
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: 0,
+
 });
 var swiper2 = new Swiper(".bg-slider", {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  loop: true,
+  spaceBetween: 0,
   thumbs: {
     swiper: swiper,
   },
